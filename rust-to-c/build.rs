51e2e10e055a569db7224cf032b6a5b558e39379
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-03-08 07:31:07 -0500

    rust-to-c:square test

--- rust-to-c/build.rs
+++ rust-to-c/build.rs
@@ -1,10 +1,6 @@
 extern crate cc;
 
 fn main() {
-    cc::Build::new()
-        .file("src/double.c")
-        .compile("libdouble.a");
-    cc::Build::new()
-        .file("src/square.c")
-        .compile("libsquare.a");
+    cc::Build::new().file("src/double.c").compile("libdouble.a");
+    cc::Build::new().file("src/square.c").compile("libsquare.a");
 }
--- rust-to-c/src/main.rs
+++ rust-to-c/src/main.rs
@@ -1,16 +1,38 @@
 extern crate libc;
 
-extern {
+extern "C" {
     fn double_input(input: libc::c_int) -> libc::c_int;
 }
-extern {
+extern "C" {
     fn square_input(input: libc::c_int) -> libc::c_int;
 }
 
 fn main() {
-    let input = 4;
+    let input = 3;
     let output = unsafe { double_input(input) };
     println!("{} * 2 = {}", input, output);
     let output = unsafe { square_input(input) };
-    println!("{} * {} = {}", input,input,  output);
+    println!("{} * {} = {}", input, input, output);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*; // Access functions from src/lib.rs
+
+    #[test]
+    fn test_double() {
+        assert_eq!(unsafe { double_input(2) }, 4);
+    } // Test add function with specific inputs
+    #[test]
+    fn test_double_fail() {
+        assert_ne!(unsafe { double_input(2) }, 5);
+    } // Test add function with specific inputs
+    #[test]
+    fn test_square() {
+        assert_eq!(unsafe { square_input(3) }, 9);
+    } // Test for non-equality as well
+    #[test]
+    fn test_square_fail() {
+        assert_ne!(unsafe { square_input(3) }, 10);
+    } // Test for non-equality as well
 }
